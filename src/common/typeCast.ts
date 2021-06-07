4e37c1764ca504550782ed677c25d99677197ecd
Johannes Schacht <johannes@sch-8.de>
2021-06-07 21:42:10 +0200

    finetuning

--- src/common/typeCast.ts
+++ src/common/typeCast.ts
@@ -1,58 +1,57 @@
 export type TypeCastConverter = (s: string) => any;
 export type TypeCastConverterList = {
-  [key: string]: TypeCastConverter;
+    [key: string]: TypeCastConverter;
 };
 // tslint:disable-next-line: variable-name
 export const typeCastConverter_Date: TypeCastConverter = (s: string) => {
-  if (isNaN(Date.parse(s))) throw new Error("not a valid Date");
-  return new Date(s);
+    if (isNaN(Date.parse(s))) throw new Error('not a valid Date');
+    return new Date(s);
 };
 
 export const typeCast = (
-  masterObject: object,
-  newObject: object,
-  converters: TypeCastConverterList = {}
+    masterObject: object,
+    newObject: object,
+    converters: TypeCastConverterList = {}
 ): object => {
-  const funcName = typeCast.name;
-  const result = { ...masterObject };
+    const funcName = typeCast.name;
+    const result = { ...masterObject };
 
-  for (const key of Object.keys(newObject)) {
-    if (!(key in masterObject))
-      throw new Error(`${funcName}: ${key} -> not found`);
-    // @ts-expect-error
-    const masterType = typeof masterObject[key];
-    // @ts-expect-error
-    const stringValue = newObject[key].toString();
-    let newValue = stringValue;
-
-    if (key in converters) {
-      try {
+    for (const key of Object.keys(newObject)) {
+        if (!(key in masterObject)) throw new Error(`${funcName}: ${key} -> not found`);
         // @ts-expect-error
-        result[key] = converters[key](stringValue);
-      } catch (e) {
-        throw new Error(`${funcName}: ${key}:${stringValue} -> ${e.message}`);
-      }
-      continue;
-    }
+        const masterType = typeof masterObject[key];
+        // @ts-expect-error
+        const stringValue = newObject[key].toString();
+        let newValue = stringValue;
 
-    switch (masterType) {
-      case "string":
-        break;
-      case "number":
-        newValue = parseInt(stringValue, 10);
-        if (isNaN(stringValue))
-          throw new Error(`${funcName}: ${key}:${stringValue} -> not a number`);
-        break;
-      case "boolean":
-        newValue = stringValue === "true";
-        break;
-      default:
-        throw new Error(
-          `${funcName}: ${key}:${stringValue} -> type \'${masterType}\' not supported`
-        );
+        if (key in converters) {
+            try {
+                // @ts-expect-error
+                result[key] = converters[key](stringValue);
+            } catch (e) {
+                throw new Error(`${funcName}:${key}: "${stringValue}" -> ${e.message}`);
+            }
+            continue;
+        }
+
+        switch (masterType) {
+            case 'string':
+                break;
+            case 'number':
+                newValue = parseInt(stringValue, 10);
+                if (isNaN(stringValue))
+                    throw new Error(`${funcName}:${key}: "${stringValue}" -> not a number`);
+                break;
+            case 'boolean':
+                newValue = stringValue === 'true';
+                break;
+            default:
+                throw new Error(
+                    `${funcName}: ${key}:${stringValue} -> type \'${masterType}\' not supported`
+                );
+        }
+        // @ts-expect-error
+        result[key] = newValue;
     }
-    // @ts-expect-error
-    result[key] = newValue;
-  }
-  return result;
+    return result;
 };
