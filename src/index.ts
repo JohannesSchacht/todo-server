4c501bd99ab07dd4aa7c50fa9b3aa24a34694558
Johannes Schacht <johannes@sch-8.de>
2021-06-07 21:14:04 +0200

    termination message

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,9 @@ import http from 'http';
 setNamespace('server');
 
 const httpServer = http.createServer(app);
-httpServer.listen(PORT, HOSTNAME, () => console.log(`Server is running ${HOSTNAME}:${PORT}`));
+try {
+    httpServer.listen(PORT, HOSTNAME, () => console.log(`Server is running ${HOSTNAME}:${PORT}`));
+} catch (e) {
+    console.log(`unexpected server termination: ${e.message}`);
+}
+console.log(`regular server termination`);
