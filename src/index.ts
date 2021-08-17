2d247bdeafb30609aeaa6bcc6c6450b1e7e2fb7a
Johannes Schacht <johannes@sch-8.de>
2021-08-17 10:11:56 +0200

    version number introduced

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,11 @@ import { setNamespace } from './middleware/logging.middelware';
 import http from 'http';
 
 setNamespace('server');
+const version = process.env.SERVER_HOSTNAME;
 
 process.on('uncaughtException', (msg) => console.error(`uncaught exception: ${msg}`));
 
 const httpServer = http.createServer(app);
-httpServer.listen(PORT, HOSTNAME, () => console.log(`Server is running -> ${HOSTNAME}:${PORT}`));
+httpServer.listen(PORT, HOSTNAME, () =>
+    console.log(`Server version ${version} is running -> ${HOSTNAME}:${PORT}`)
+);
